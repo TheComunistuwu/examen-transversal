ca5866e569b8e0f1a53d1b58e09716f2b2066add
TheComunistuwu <benj.munoz@duocuc.cl>
2022-07-08 20:56:35 -0400

    init

---
+++ core/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm
+from .models import *
+class productoForm(ModelForm):
+    class Meta:
+        model =producto 
+        fields = ['idProducto','nombreProducto','stockProducto','precioProducto','imagenProducto','descuento_aplicado']
+
+
+
+class promocionesForm(ModelForm):
+    class Meta:
+        model = descuento
+        fields = ['idDescuento','nombreDescuento','cantidadDescuento','estado']
+
+
+
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py sqlmigrate core 0001
+# python manage.py sqlmigrate core 0002
+# python manage.py runserver
